ed6ab91968a0da358b6a827d485d6064faa74b78
MerabTskhvedadze <merabi.tskhvedadze@gmail.com>
2023-03-23 21:40:22 +0400

    displayed tasks in a list

--- src/components/TaskList/TaskItem/TaskItem.jsx
+++ src/components/TaskList/TaskItem/TaskItem.jsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import { useState } from 'react';
+
+import {
+  CheckIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from '@heroicons/react/24/outline';
+
+import styles from './TaskItem.module.css';
 
 export const TaskItem = ({ task }) => {
-  return <div>{task.value}</div>;
+  const [isChecked, setIsChecked] = useState(task.checked);
+
+  const handleCheckboxChange = () => {
+    setIsChecked((prev) => !prev);
+  };
+
+  return (
+    <li className={styles.task}>
+      <div className={styles['task-group']}>
+        <input
+          type='checkbox'
+          className={styles.checkbox}
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+          name={task.name}
+          id={task.id}
+        />
+        <label htmlFor={task.id} className={styles.label}>
+          {task.value}
+          <p className={styles.checkmark}>
+            <CheckIcon strokeWidth={2} width={24} height={24} />
+          </p>
+        </label>
+      </div>
+      <div className={styles['task-group']}>
+        <button
+          className='btn'
+          aria-label={`Update ${task.name} Task`}
+          // onClick={() => enterEditMode(task)}
+        >
+          <PencilSquareIcon width={24} height={24} />
+        </button>
+        <button
+          className={`btn ${styles.delete}`}
+          aria-label={`Delete ${task.name} Task`}
+          // onClick={() => deleteTask(task.id)}
+        >
+          <TrashIcon width={24} height={24} />
+        </button>
+      </div>
+    </li>
+  );
 };
